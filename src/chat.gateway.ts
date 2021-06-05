9624892c1426510ec718958ec6af5b2ed9eebcde
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-06-04 23:28:05 -0500

    :dart: Improve: agregar socket en servidor y crear cliente con socketsio

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,4 +1,17 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 
-@WebSocketGateway(80, { namespace: 'chat' })
-export class ChatGateway {}
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer()
+  server;
+  //Puedes acceder al cliente utilizando (client, data)
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    this.server.emit('message', message);
+  }
+}
